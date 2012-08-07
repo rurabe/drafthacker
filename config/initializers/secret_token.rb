a8c7983029a5779c48fa25499cf1da77149b73f1
DBC Apprentice <apprentice@devbootcamp.com>
2012-08-06 17:16:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drafthacker::Application.config.secret_token = '82b0b87fb158794c5195a9eab1096a3cc16a23e61446cc928623ca9d6662f60cd1dd23083f6b576e8862277de0a167e43f101deb20e33e3e5c1e1b04b601843a'
