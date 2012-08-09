833e456ba5d01294b98552de376eeca18b32636f
rurabe <rurabe@gmail.com>
2012-08-09 16:45:52 -0700

    fixed controller

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -2,7 +2,7 @@ class DraftsController < ApplicationController
   include CbsClient
 
   def show
-    @params = get_params(params['access_key'], params['user_id'])
+    @params = get_params(params['access_token'], params['user_id'])
   end
 
   def index
