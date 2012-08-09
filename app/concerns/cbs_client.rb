b5b557bd315c5130907b349d0003efe4ee99bb1a
Noah Shafi + Ryan Urabe <apprentice+ns+ru@devbootcamp.com>
2012-08-09 16:30:32 -0700

    Fixed module

--- app/concerns/cbs_client.rb
+++ app/concerns/cbs_client.rb
@@ -1,13 +1,16 @@
 module CbsClient
 
-    def get_params(access_token,cbs_id)
-      @@access_token = access_token
-      @@cbs_id = cbs_id
+require 'json'
+require 'open-uri'
 
-      build_mega_hash
-    end
- 
-    def populate_players
+  def get_params(access_token,cbs_id)
+    @@access_token = access_token
+    @@cbs_id = cbs_id
+
+    build_mega_hash
+  end
+
+  def populate_players
       parsed_data = parse('http://api.cbssports.com/fantasy/players/list?SPORT=football&response_format=JSON')
       all_players = parsed_data[:body][:players]
 
@@ -35,48 +38,50 @@ module CbsClient
 
   private
 
-    def convert_keys(hash, key_conversion_hash)
-      new_hash = {}
-      hash.each do |k, v|
-        new_hash[key_conversion_hash[k]] = hash[k]
-      end
-      new_hash
+  def convert_keys(hash, key_conversion_hash)
+    new_hash = {}
+    hash.each do |k, v|
+      new_hash[key_conversion_hash[k]] = hash[k]
     end
+    new_hash
+  end
 
 
-    def create_player_by_position_type(position, attributes)
+  def create_player_by_position_type(position, attributes)
 
-      position_to_object_hash = {
-                                  "D"         => lambda { Defense.create(attributes) },
-                                  "DB"        => lambda { DefensiveBack.create(attributes) },
-                                  "DL"        => lambda { DefensiveLineman.create(attributes) },
-                                  "DST"       => lambda { DefenseSpecialTeams.create(attributes) },
-                                  "K"         => lambda { Kicker.create(attributes) },
-                                  "LB"        => lambda { Linebacker.create(attributes) },
-                                  "QB"        => lambda { Quarterback.create(attributes) },
-                                  "RB"        => lambda { RunningBack.create(attributes) },
-                                  "ST"        => lambda { SpecialTeams.create(attributes) },
-                                  "TE"        => lambda { TightEnd.create(attributes) },
-                                  "TQB"       => lambda { TeamQuarterback.create(attributes) },
-                                  "WR"        => lambda { WideReceiver.create(attributes) },
-                                  "FLEX"      => lambda { Player.create(attributes) },
-                                  "ID"        => lambda { Player.create(attributes) },
-                                  "WR-TE"     => lambda { Player.create(attributes) },
-                                  "RB-WR"     => lambda { Player.create(attributes) },
-                                  "RB-WR-TE"  => lambda { Player.create(attributes) },
-                                  "DL-LB-DB"  => lambda { Player.create(attributes) }
-                                }
+    position_to_object_hash = {
+                                "D"         => lambda { Defense.create(attributes) },
+                                "DB"        => lambda { DefensiveBack.create(attributes) },
+                                "DL"        => lambda { DefensiveLineman.create(attributes) },
+                                "DST"       => lambda { DefenseSpecialTeams.create(attributes) },
+                                "K"         => lambda { Kicker.create(attributes) },
+                                "LB"        => lambda { Linebacker.create(attributes) },
+                                "QB"        => lambda { Quarterback.create(attributes) },
+                                "RB"        => lambda { RunningBack.create(attributes) },
+                                "ST"        => lambda { SpecialTeams.create(attributes) },
+                                "TE"        => lambda { TightEnd.create(attributes) },
+                                "TQB"       => lambda { TeamQuarterback.create(attributes) },
+                                "WR"        => lambda { WideReceiver.create(attributes) },
+                                "FLEX"      => lambda { Player.create(attributes) },
+                                "ID"        => lambda { Player.create(attributes) },
+                                "WR-TE"     => lambda { Player.create(attributes) },
+                                "RB-WR"     => lambda { Player.create(attributes) },
+                                "RB-WR-TE"  => lambda { Player.create(attributes) },
+                                "DL-LB-DB"  => lambda { Player.create(attributes) }
+                              }
 
-      position_to_object_hash[position].call
+    position_to_object_hash[position].call
 
-    end
+  end
 
-    def parse(path)
-      data = open(path).read
-      JSON.parse(data, :symbolize_names => true)
-    end
+  def parse(path)
+    data = open(path).read
+    JSON.parse(data, :symbolize_names => true)
+  end
 
-    def build_mega_hash
+  private
+
+  def build_mega_hash
     {:user => @@cbs_id,
       :drafts_attributes =>
         [
@@ -89,71 +94,71 @@ module CbsClient
           }
         ]
     }
-    end
 
-    def build_hash_league_details
-      response = json_response('league/details')[:body][:league_details]
-      response[:commish_type] = response[:type]
-      response.delete :type
-      @@league_details = response
-    end
+  end
 
-    def build_hash_draft_config
-      response = json_response('league/draft/config')[:body][:draft]
-      response[:draft_event_type] = response[:type]
-      response.delete :type
-      @@draft_config = response
-    end
+  def build_hash_league_details
+    response = json_response('league/details')[:body][:league_details]
+    response[:commish_type] = response[:type]
+    response.delete :type
+    @@league_details = response
+  end
 
-    def build_hash_draft_order
-      response = json_response('league/draft/order')[:body][:draft_order][:picks]
-      rounds = []
-      @@draft_config[:rounds].to_i.times do |i|
-        rounds << { :number => i+1, :picks_attributes =>  response.select { |pick| pick[:round] == i+1 } }
+  def build_hash_draft_config
+    response = json_response('league/draft/config')[:body][:draft]
+    response[:draft_event_type] = response[:type]
+    response.delete :type
+    @@draft_config = response
+  end
+
+  def build_hash_draft_order
+    response = json_response('league/draft/order')[:body][:draft_order][:picks]
+    rounds = []
+    @@draft_config[:rounds].to_i.times do |i|
+      rounds << { :number => i+1, :picks_attributes =>  response.select { |pick| pick[:round] == i+1 } }
+    end
+    rounds
+  end
+
+  def build_hash_fantasy_teams
+    response = json_response('league/teams')[:body][:teams]
+    new_hash = {}
+    response.map! do |team|
+      team.merge build_slots_array
+    end
+    response
+  end
+
+  def build_hash_league_rules
+    json_response('league/rules')[:body][:rules]
+  end
+
+  def build_slots_array
+    response = build_hash_league_rules
+    slots_array = []
+    response[:roster][:positions].each do |hash|
+      hash[:max_active].to_i.times do
+        slots_array << { :eligible_position => hash[:abbr] }
       end
-      rounds
     end
+    response[:roster][:statuses][1][:max].to_i.times do
+      slots_array << { :eligible_position => "RS" }
+    end
+    { :slots_attributes => slots_array }
+  end
 
-    def build_hash_fantasy_teams
-      response = json_response('league/teams')[:body][:teams]
-      new_hash = {}
-      response.map! do |team|
-        team.merge build_slots_array
-      end
-      response
-    end
+  def json_response(api_call)
+    JSON.parse(read_response(api_call), :symbolize_names => true)
+  end
 
-    def build_hash_league_rules
-      json_response('league/rules')[:body][:rules]
-    end
+  def read_response(api_call)
+    open(build_url(api_call)).read
+  end
 
-    def build_slots_array
-      response = build_hash_league_rules
-      slots_array = []
-      response[:roster][:positions].each do |hash|
-        hash[:max_active].to_i.times do
-          slots_array << { :eligible_position => hash[:abbr] }
-        end
-      end
-      response[:roster][:statuses][1][:max].to_i.times do
-        slots_array << { :eligible_position => "RS" }
-      end
-      { :slots_attributes => slots_array }
-    end
-
-    def json_response(api_call)
-      JSON.parse(read_response(api_call), :symbolize_names => true)
-    end
-
-    def read_response(api_call)
-      open(build_url(api_call)).read
-    end
-
-    def build_url(api_call)
-      "http://api.cbssports.com/fantasy/#{api_call}?access_token=#{@@access_token}&response_format=JSON"
-    end
+  def build_url(api_call)
+    "http://api.cbssports.com/fantasy/#{api_call}?access_token=#{@@access_token}&response_format=JSON"
+  end
 end
-
 # def rounds_and_picks
 #   parsed_data = parse('http://api.cbssports.com/fantasy/league/draft/order?SPORT=football&response_format=JSON&access_token=U2FsdGVkX1-Sj855Oa9QwS41LUcDCCaFkEAYBU_-htOX1VAMnUzbo4ZFtx_Y_EflpOxk9ztMxFQD2bCvNGeJ5NK68I3-Az4ni1Dans02_yyVxIBI_-BUAKifINXHnrvz')
 #   picks_data = parsed_data[:body][:draft_order][:picks]
--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -2,7 +2,7 @@ class DraftsController < ApplicationController
   include CbsClient
 
   def show
-    @params = get_params('U2FsdGVkX1-r3rVlygny0MK8WI1QW9Kb9IZRO0tiz3YBzDltmdjVumhYsQ_mue54lQSG31A45CPBQfof1Xz6aRAmhsPH4CBaAmyuPS7wPdwwhfD78_xQYxL0E07-L8Ih', 'b2c7c77e1b22e0f4')
+    @params = get_params(params['access_key'], params['user_id'])
   end
 
   def index
