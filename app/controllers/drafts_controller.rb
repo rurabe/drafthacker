3704b8ff2d67d088693b8b91c2025f73a00bc533
rurabe <rurabe@gmail.com>
2012-08-10 00:14:46 -0700

    edited the view

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -3,7 +3,9 @@ class DraftsController < ApplicationController
   def show
     @user = User.new(Cbs::League.params(params['access_token'], params['user_id']))
     @user.save
-    audit "SHIIT"
+    @draft = @user.drafts.first
+    @league = @draft.league
+    @team = @user.team
   end
 
   def index
