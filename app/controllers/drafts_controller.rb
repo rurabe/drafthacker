e7dd3e7c75380df864d054cbf440644508df14a7
Noah Shafi + Ryan Urabe <apprentice+ns+ru@devbootcamp.com>
2012-08-10 12:03:27 -0700

    in the midst of helping local development

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,7 +1,11 @@
 class DraftsController < ApplicationController
 
   def show
-  	@mega_hash = Cbs::League.params(params['access_token'], params['user_id'])
+    if params['access_token']
+      @mega_hash = Cbs::League.params(params['access_token'], params['user_id'])
+    else
+      @mega_hash = Cbs::League.params()
+    end
     @user = User.new(@mega_hash)
     @user.save
     @draft = @user.drafts.first
