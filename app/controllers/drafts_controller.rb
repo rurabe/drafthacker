0415e5a52e413c219213ccf9ccebfbb16e661d65
Noah Shafi + Ryan Urabe <apprentice+ns+ru@devbootcamp.com>
2012-08-09 18:32:26 -0700

    Shit is good. Got that Module semi cleaned son

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,8 +1,7 @@
 class DraftsController < ApplicationController
-  include CbsClient
 
   def show
-    @params = get_params(params['access_token'], params['user_id'])
+    @params = Cbs::League.params('U2FsdGVkX1-r3rVlygny0MK8WI1QW9Kb9IZRO0tiz3YBzDltmdjVumhYsQ_mue54lQSG31A45CPBQfof1Xz6aRAmhsPH4CBaAmyuPS7wPdwwhfD78_xQYxL0E07-L8Ih', 'b2c7c77e1b22e0f4')
   end
 
   def index
