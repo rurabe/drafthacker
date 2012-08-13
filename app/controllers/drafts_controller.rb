5a2446caa27b910b04bd7ceb51f7519f67c130da
Noah Shafi + Ryan Urabe <apprentice+ns+ru@devbootcamp.com>
2012-08-13 16:19:59 -0700

    added to ui

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -3,9 +3,11 @@ class DraftsController < ApplicationController
   def show
 
     if params['access_token']
-      @mega_hash = Cbs::League.build_mega_hash( { :access_token => params['access_token'], :cbs_id => params['user_id'] } )
+      @access = params['access_token']
+      @mega_hash = Cbs::League.build_mega_hash( { :access_token => @access, :cbs_id => params['user_id'] } )
     else
-      @mega_hash = Cbs::League.build_mega_hash( { :access_token => 'U2FsdGVkX1_dqYNjfcxVhErfEI9MqM7-7hNf7v6d64EzPGAjzKpHFAe8cA3jHI2G_KARduk8IlB7L6WNbPbzlXCz0vpOfb9VebWtJJj2A_mPprAYtwtKY1BeMDB_XcdW', :cbs_id => 'b2c7c77e1b22e0f4' } )
+      @access = 'U2FsdGVkX1_dqYNjfcxVhErfEI9MqM7-7hNf7v6d64EzPGAjzKpHFAe8cA3jHI2G_KARduk8IlB7L6WNbPbzlXCz0vpOfb9VebWtJJj2A_mPprAYtwtKY1BeMDB_XcdW'
+      @mega_hash = Cbs::League.build_mega_hash( { :access_token => @access, :cbs_id => 'b2c7c77e1b22e0f4' } )
     end
 
     @user = User.new(@mega_hash)
