487592073fbddd53565853e651e479a8a5c94717
Noah Shafi + Ryan Urabe <apprentice+ns+ru@devbootcamp.com>
2012-08-13 18:03:01 -0700

    more css lined everything up

--- db/schema.rb
+++ db/schema.rb
@@ -79,15 +79,15 @@ ActiveRecord::Schema.define(:version => 20120812214805) do
   add_index "picks", ["team_id"], :name => "index_picks_on_team_id"
 
   create_table "players", :force => true do |t|
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.string   "type"
     t.integer  "cbs_id"
     t.string   "first_name"
     t.string   "full_name"
     t.string   "last_name"
     t.string   "on_waivers"
-    t.string   "primary_position"
+    t.string   "position"
     t.string   "pro_status"
     t.string   "pro_team"
     t.string   "bye_week"
@@ -95,6 +95,14 @@ ActiveRecord::Schema.define(:version => 20120812214805) do
     t.string   "opponent"
     t.integer  "slot_id"
     t.string   "icons"
+    t.integer  "high"
+    t.integer  "low"
+    t.string   "profile_link"
+    t.integer  "pct"
+    t.integer  "change"
+    t.integer  "avg"
+    t.string   "profile_url"
+    t.integer  "rank"
   end
 
   add_index "players", ["slot_id"], :name => "index_players_on_slot_id"
