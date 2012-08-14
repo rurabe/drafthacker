f9e2871c95ca88660afc310f311943b15e9160de
rurabe <rurabe@gmail.com>
2012-08-14 03:30:26 -0700

    got most of the auction values working

--- app/concerns/cbs.rb
+++ app/concerns/cbs.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 module Cbs
 
-  module ApiCall
+  module ApiCall #--------------------------------------------------------------------------------------------------------
 
     def json_response(options = {}) #eg { :api_call => 'league/details', :params => { :access_token => "csugvwu3298hfw9" } }
       JSON.parse(read_response(options), :symbolize_names => true)
@@ -29,7 +29,7 @@ module Cbs
       end
   end
 
-  class Draft
+  class Draft #--------------------------------------------------------------------------------------------------------
     extend ApiCall
 
     def self.update( options = {} ) #eg. { :access_token => "vksdhgvkhsdbvksdiugweiufgwiusd", :draft_id => 6362 }
@@ -57,7 +57,7 @@ module Cbs
     end
   end
 
-  class Players
+  class Players #--------------------------------------------------------------------------------------------------------
     extend ApiCall
     # Populates the db with all players or updates them if they already exist. Approximately 2827 total. 
     def self.populate
@@ -70,12 +70,19 @@ module Cbs
       update_or_create(players)
     end
 
+    def self.populate_auction_values
+      update_or_create(build_auction_values,false)
+    end
+
+
+
+
     private
-      def self.update_or_create(players)
+      def self.update_or_create(players,needs_cleaning=true)
         # Take each player hash
         players.each do |player|
           # Clean the hash for reserved words and to match our schema
-          clean_hash(player)
+          clean_hash(player) if needs_cleaning
           # Where tries to find the object in the current records. If nil, it creates a new one.
           player_obj = Player.where(:id => player[:id]).first_or_initialize(player)
           # If the record is not new, run update attributes on it.
@@ -98,11 +105,30 @@ module Cbs
         hash
       end
 
+      def self.build_auction_values
+        # The various types of auction values available to us. 
+        sources = ['cbs','cbs_ppr','dave_richard','dave_richard_ppr','jamey_eisenberg','jamey_eisenberg_ppr','nathan_zegura']
+        # An empty array for the results
+        auction_values = []
+        # So for each of these sources:
+        sources.each do |source|
+          # Get the data. It's in a weird format eg. {"187741": "23", "396811": "11"}
+          data = json_response( { :api_call => 'auction-values', :params => { :SPORT => "football",:source => source } } )[:body][:auction_values]
+          # Convert it into a more useful format
+          data.each do |k,v|
+            # Put these more useful hashes into the array
+            auction_values << {:id => k, "av_#{source}".to_sym => v }
+          end
+        end
+        # Return the array
+        auction_values
+      end
+
   end
 
 
 
-  class League
+  class League #--------------------------------------------------------------------------------------------------------
     extend ApiCall
 
     def self.build_mega_hash(options = {}) # eg. { :access_token => "csugvwu3298hfw9", :cbs_id => "r29hefb298f2b" }
--- app/models/player.rb
+++ app/models/player.rb
@@ -10,18 +10,26 @@ class Player < ActiveRecord::Base
                   :icons_injury,
                   :icons_headline,
                   :last_name,
-                  :on_waivers,         # Is this neccesary?   
+                  :on_waivers,             # Is this neccesary?   
                   :pro_status,
                   :pro_team,
                   :bye_week,
                   :is_locked,
                   :opponent,
-                  :pct,                # ADP
-                  :change,             # ADP
-                  :avg,                # ADP
+                  :pct,                    # ADP
+                  :change,                 # ADP
+                  :avg,                    # ADP
                   :position,           
-                  :high,               # ADP
-                  :low,                # ADP
-                  :rank                # ADP
+                  :high,                   # ADP
+                  :low,                    # ADP
+                  :rank,                   # ADP
+                  :av_cbs,                 # Auction Values
+                  :av_cbs_ppr,             # Auction Values
+                  :av_dave_richard,        # Auction Values
+                  :av_dave_richard_ppr,    # Auction Values
+                  :av_jamey_eisenberg,     # Auction Values
+                  :av_jamey_eisenberg_ppr, # Auction Values
+                  :av_nathan_zegura        # Auction Values
+
 
 end
