9e32978e8cf39228c99caa25d0351350987c8c64
rurabe <rurabe@gmail.com>
2012-08-15 02:25:46 -0700

    made linking much much faster

--- app/models/player.rb
+++ app/models/player.rb
@@ -31,16 +31,5 @@ class Player < ActiveRecord::Base
                   :av_jamey_eisenberg_ppr, # Auction Values
                   :av_nathan_zegura        # Auction Values
 
-  def self.drafted(options = {}) #eg. {:draft_id => 3}
-    draft = Draft.find(options.fetch(:draft_id))
-    drafted_player_ids = draft.picks.order('picks.number').pluck(:player_id)
-
-    Player.where(:id => drafted_player_ids)
-  end
-
-  def self.undrafted(options = {}) #eg. {:draft_id => 3}
-    Player.all - drafted(options)
-  end
-
 
 end
