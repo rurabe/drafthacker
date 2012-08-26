26f571cfff952a227eec4d47c9d19e91ce6085a5
sungchoi <apprentice@devbootcamp.com>
2012-08-26 14:15:43 -0700

    additional notes

--- app/concerns/updates_and_inserts.rb
+++ app/concerns/updates_and_inserts.rb
@@ -1,5 +1,6 @@
 module UpdatesAndInserts
 
+
   def upsert(activerecord_subclass, array_of_hashes, index_column)
       if array_of_hashes.class == Array
         Upsert.batch(activerecord_subclass.connection, activerecord_subclass.table_name) do |upsert|
@@ -14,5 +15,19 @@ module UpdatesAndInserts
         # :error => "you must provide an array of attribute hashes"
       end
     end
+
+
+    # extend ApiCall
+    # players = json_response( { :api_call => 'players/list', :params => { :SPORT => "football" } } )[:body][:players]
+    # players.map! do |player|
+    #   player[:created_at] = Time.now # upsert does not seem to handle created_at and updated_at
+    #   player[:updated_at] = Time.now # it errors if either are missing, whether or not the row exists already
+    #   player[:cbs_id] = player.delete(:id)
+    #   player[:icons_injury] = player[:icons][:injury] if player[:icons][:injury]
+    #   player[:icons_headline] = player[:icons][:headline] if player[:icons][:headline]
+    #   # player.delete(:icons)
+    # end
+    # upsert(Player, players, :id)
+
 
 end
