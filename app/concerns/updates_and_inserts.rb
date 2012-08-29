90887417c71f2b70e9954b9f33be90a4525f9999
Sung Choi <m.sung.choi@gmail.com>
2012-08-29 00:25:19 -0700

    fixes sanitization code for upsert_players. upsert_players now works properly!

--- app/concerns/updates_and_inserts.rb
+++ app/concerns/updates_and_inserts.rb
@@ -1,33 +1,36 @@
 module UpdatesAndInserts
 
 
-  def upsert(activerecord_subclass, array_of_hashes, index_column)
+  def self.upsert(activerecord_subclass, array_of_hashes, index_column)
       if array_of_hashes.class == Array
         Upsert.batch(activerecord_subclass.connection, activerecord_subclass.table_name) do |upsert|
           array_of_hashes.each do |hash|
-            binding.pry
             upsert.row({index_column => hash.delete(index_column)}, hash) # This syntax is fine.
-            binding.pry
           end
-          binding.pry
         end
       else
         # :error => "you must provide an array of attribute hashes"
       end
     end
 
-
-    # extend ApiCall
-    # players = json_response( { :api_call => 'players/list', :params => { :SPORT => "football" } } )[:body][:players]
-    # players.map! do |player|
-    #   player[:created_at] = Time.now # upsert does not seem to handle created_at and updated_at
-    #   player[:updated_at] = Time.now # it errors if either are missing, whether or not the row exists already
-    #   player[:cbs_id] = player.delete(:id)
-    #   player[:icons_injury] = player[:icons][:injury] if player[:icons][:injury]
-    #   player[:icons_headline] = player[:icons][:headline] if player[:icons][:headline]
-    #   # player.delete(:icons)
-    # end
-    # upsert(Player, players, :id)
-
+  def self.upsert_players
+    extend ApiCall
+    players = json_response( { :api_call => 'players/list', :params => { :SPORT => "football" } } )[:body][:players]
+    players.map! do |player|
+      player[:created_at] = Time.now # upsert does not seem to handle created_at and updated_at
+      player[:updated_at] = Time.now # it errors if either are missing, whether or not the row exists already
+      player[:cbs_id] = player.delete(:id)
+      
+      if player[:icons]
+      player[:icons_injury] = player[:icons][:injury] if player[:icons][:injury]
+      player[:icons_headline] = player[:icons][:headline] if player[:icons][:headline]
+      player.delete(:icons)
+      end
+      
+      player
+    end
+    upsert(Player, players, :cbs_id)
+    puts "Player.count = #{Player.count}"
+  end
 
 end
